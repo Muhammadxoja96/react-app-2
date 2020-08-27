7953a88175675e608fd869755f39e473486a154e
Muhammadxoja96 <70304635+Muhammadxoja96@users.noreply.github.com>
2020-08-27 10:30:16 +0500

    react v1

---
+++ precache-manifest.d38f6c570816bc02e019f59ba0489a2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e035d59243848b7205",
+    "url": "/static/css/main.7d213612.chunk.css"
+  },
+  {
+    "revision": "08e035d59243848b7205",
+    "url": "/static/js/main.b11889c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd84fb9fdba77ef43b9d361c8df06298",
+    "url": "/index.html"
+  }
+];
